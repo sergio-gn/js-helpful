a890e57f59d9b4d26096665811f27abde98c191d
Sergio Garcia Neto <88346250+sergio-gn@users.noreply.github.com>
2023-06-26 20:01:00 +1000

    argument

---
+++ argument.js
@@ -0,0 +1,12 @@
+function greet(name) {
+    console.log("In this case, " + name + " inside the parameter placeholder when you call the function is the Argument");
+}
+
+greet("John");
+
+
+function addNumbers(a, b) {
+    console.log(a + b);
+}
+
+addNumbers(5, 7);
